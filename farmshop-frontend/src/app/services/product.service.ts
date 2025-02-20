63ee28605352f6329136320ffb6a49073ac15f06
Soufiane MEFTAH <s.mef2703@gmail.com>
2025-02-20 09:03:28 +0100

    Added ProductService - Fetching Products from API

--- farmshop-frontend/src/app/services/product.service.ts
+++ farmshop-frontend/src/app/services/product.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = 'http://127.0.0.1:8000/api/products/'; // URL de l'API Backend
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Récupérer tous les produits depuis le backend
+   */
+  getProducts(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
