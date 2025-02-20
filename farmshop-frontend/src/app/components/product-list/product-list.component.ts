0b8b482e7b25a36514ffa386f1caedfda2e8b82b
Soufiane MEFTAH <s.mef2703@gmail.com>
2025-02-20 09:11:56 +0100

    Added ProductListComponent and integrated ProductService

--- farmshop-frontend/src/app/components/product-list/product-list.component.ts
+++ farmshop-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service'; // Import du service
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: any[] = []; // Stocker les produits
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getProducts().subscribe(
+      (data) => {
+        this.products = data;
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
 }
