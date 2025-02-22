7f2063851c134d7a6b8683385b8a4101a83628d8
Soufiane MEFTAH <s.mef2703@gmail.com>
2025-02-22 10:51:33 +0100

    💡 Improved navbar visibility and styling

--- farmshop-frontend/src/app/app.routes.ts
+++ farmshop-frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) }, // Accueil
-  { path: 'products', loadComponent: () => import('./components/product-list/product-list.component').then(m => m.ProductListComponent) }, // Produits
-  { path: 'blog', loadComponent: () => import('./components/blog/blog.component').then(m => m.BlogComponent) }, // Blog
-  { path: 'contact', loadComponent: () => import('./components/contact/contact.component').then(m => m.ContactComponent) }, // Contact
-  { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) }, // Connexion
-  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirection vers l'accueil en cas d'erreur
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirection vers l'accueil
 ];
